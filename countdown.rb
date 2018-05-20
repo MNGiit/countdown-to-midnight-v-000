28e788cc85481e644209f81a8933f02b92f0fa23
Mohammed Noori <kmanoorib@gmail.com>
2018-05-20 08:53:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,12 @@
 #write your code here
 
 def countdown(number)
+  count = number
   until number == 0
     puts "#{number} SECOND(S)!"
     number-=1
+  end
+  if count == 12
+    puts "HAPPY NEW YEARS!"
   end
 end
